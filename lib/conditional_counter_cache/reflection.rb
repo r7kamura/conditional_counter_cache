ef635681b0aa9f231edb75a7600ac345b66d0f38
Ryo Nakamura <r7kamura@gmail.com>
2014-10-07 18:31:57 +0900

    Fix non counter-cached belongs_to association

--- lib/conditional_counter_cache/reflection.rb
+++ lib/conditional_counter_cache/reflection.rb
@@ -2,13 +2,12 @@ module ConditionalCounterCache
   module Reflection
     # @note Overridden
     def counter_cache_column
-      Option.new(options[:counter_cache]).column_name ||
-        "#{active_record.name.demodulize.underscore.pluralize}_count"
+      Option.new(self).column_name
     end
 
     # @return [true, false]
     def has_countable?(record)
-      Option.new(options[:counter_cache]).condition.call(record)
+      Option.new(self).condition.call(record)
     end
 
     # Utility wrapper of `option[:counter_cache][:condition]`.
@@ -30,22 +29,24 @@ module ConditionalCounterCache
       end
     end
 
-    # Utility wrapper of `option[:counter_cache]`.
+    # Utility wrapper of reflection to process `option[:counter_cache]`.
     class Option
-      # @param [Hash, String, true, nil]
-      def initialize(value)
-        @value = value
+      # @param [ActiveRecord::Reflection]
+      def initialize(reflection)
+        @reflection = reflection
       end
 
       # @return [String, nil] Specified column name, or nil meaning "use default column name".
       def column_name
-        case
-        when has_hash_value?
-          @value[:column_name]
-        when has_true_value?
-          nil
+        case cache
+        when Hash
+          cache[:column_name]
+        when String, Symbol
+          cache.to_s
+        when true
+          "#{@reflection.active_record.name.demodulize.underscore.pluralize}_count"
         else
-          @value.to_s
+          nil
         end
       end
 
@@ -56,17 +57,13 @@ module ConditionalCounterCache
 
       private
 
-      def has_hash_value?
-        @value.is_a?(Hash)
-      end
-
-      def has_true_value?
-        @value == true
+      def cache
+        @reflection.options[:counter_cache]
       end
 
       # @return [Proc, String, Symbol, nil]
       def raw_condition
-        @value[:condition] if has_hash_value?
+        cache[:condition] if cache.is_a?(Hash)
       end
     end
   end
