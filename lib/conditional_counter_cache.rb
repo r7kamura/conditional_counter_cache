1b9289a471aab407f066373292119126329910dc
Ryo Nakamura <r7kamura@gmail.com>
2014-10-07 10:09:34 +0900

    bundle gem conditional_counter_cache

---
+++ lib/conditional_counter_cache.rb
@@ -0,0 +1,5 @@
+require "conditional_counter_cache/version"
+
+module ConditionalCounterCache
+  # Your code goes here...
+end
---
+++ lib/conditional_counter_cache/version.rb
@@ -0,0 +1,3 @@
+module ConditionalCounterCache
+  VERSION = "0.0.1"
+end
